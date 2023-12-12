ca47abd1bfdfed38b75af2727d28509cdbebbb9b
GUchechi <godswilluchendu7@gmail.com>
2023-12-12 10:42:16 +0100

    first commit

---
+++ screens/SelectCity.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SelectCity = () => {
+  return (
+    <View>
+      <Text>SelectCity</Text>
+    </View>
+  )
+}
+
+export default SelectCity
+
+const styles = StyleSheet.create({})
